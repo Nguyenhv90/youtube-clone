e9ea8b7c0bcbf945c2cefe1fa50e9a87e3e25bf1
Nguyenhv90 <nguyenhv236@gmail.com>
2023-03-14 10:17:09 +0700

    #commit code video part 8

--- fontend/youtube-clone-ui/src/app/app.module.ts
+++ fontend/youtube-clone-ui/src/app/app.module.ts
@@ -23,6 +23,7 @@ import {VgCoreModule} from '@videogular/ngx-videogular/core';
 import {VgControlsModule} from '@videogular/ngx-videogular/controls';
 import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
 import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
     VgControlsModule,
     VgOverlayPlayModule,
     VgBufferingModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ fontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { ActivatedRoute } from '@angular/router';
+import { VideoService } from 'app/video.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-save-video-details',
@@ -17,8 +20,15 @@ export class SaveVideoDetailsComponent implements OnInit{
   addOnBlur = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
   tags: string[] = [];
+  selectedFile!: File ;
+  selectedFileName = '';
+  fileUploaded = false;
+  videoId = ''
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService,
+    private snackBar: MatSnackBar) {
+
+    this.videoId = this.activatedRoute.snapshot.params['videoId'];
     this.saveVideoDetailsForm = new FormGroup({
       title: this.title,
       description: this.description,
@@ -63,6 +73,21 @@ export class SaveVideoDetailsComponent implements OnInit{
     if (index >= 0) {
       this.tags[index] = value;
     }
+  }
+
+  onFileSelected(event: Event) {
+    // @ts-ignore
+    this.selectedFile = event.target.files[0];
+    this.selectedFileName = this.selectedFile.name
+    this.fileUploaded = true;
+  }
+
+  onUpload() {
+    if(this.selectedFile) return;
+    this.videoService.uploadThumbnail(this.selectedFile, this.videoId).subscribe(data => {
+      console.log(data);
+      this.snackBar.open("Thumbnail upload successfull", "OK");
+    })
   }
 
 }
